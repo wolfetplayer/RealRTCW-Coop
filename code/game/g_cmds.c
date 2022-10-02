25df78c60bbd2e05778b3dd7e4911a0937c01a08
German <karabastov@yahoo.com>
2022-10-02 10:54:45 +0300

    Flamethrower fix

--- code/game/g_cmds.c
+++ code/game/g_cmds.c
@@ -2989,9 +2989,8 @@ void ClientDamage( gentity_t *clent, int entnum, int enemynum, int id ) {
 			}
 		}
 		break;
-#if 0 // Why was this removed again?
 	case CLDMG_FLAMETHROWER:
-		if ( ent->takedamage && !AICast_NoFlameDamage( ent->s.number ) ) {
+		if ( ( enemy->aiCharacter == AICHAR_ZOMBIE || !Q_stricmp( enemy->classname, "props_flamethrower" ) ) && ent->takedamage && !AICast_NoFlameDamage( ent->s.number ) ) {
 			#define FLAME_THRESHOLD 50
 			int damage = 5;
 
@@ -3032,7 +3031,6 @@ void ClientDamage( gentity_t *clent, int entnum, int enemynum, int id ) {
 			}
 		}
 		break;
-#endif
 	}
 }
 // -NERVE - SMF
